c447bf8e6a3a60f27537d97f500a5e4ae9007a23
mtdosterly <74225864+mtdosterly@users.noreply.github.com>
2020-11-14 01:24:46 -0600

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+#my fizzbuzz
+
+for num in range(1,101):
+    if num % 3 == 0:
+        if num % 5 == 0:
+            print("FizzBuzz")
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
