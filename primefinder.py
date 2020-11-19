3f73483f4d2d87ca4f1ead5304cce1c95ae064a5
mtdosterly <74225864+mtdosterly@users.noreply.github.com>
2020-11-19 16:28:09 -0600

    ask print/write to file before calculating

--- primefinder.py
+++ primefinder.py
@@ -1,7 +1,7 @@
 def find_primes(num):
     primes = [2]
     for val in range(3,num,2):
-        for div in range(2,val-1):
+        for div in range(2,int(val**.5)+1):
             if val % div == 0:
                 break
         else:
@@ -9,8 +9,8 @@ def find_primes(num):
     return primes
 
 limit = int(input("Enter the upper limit for determining primes: "))
-primeList = find_primes(limit)
 choice = input("Do you want me to display the numbers or save them to a file? Enter 'file' or 'display': ")
+primeList = find_primes(limit)
 if choice == 'file':
     f = open(f"primelist_{limit}", "w")
     f.write(str(primeList))
