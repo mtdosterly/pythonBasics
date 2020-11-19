abaa8e8c5254191ba604273e207cc1a6676fd1ea
mtdosterly <74225864+mtdosterly@users.noreply.github.com>
2020-11-19 13:57:38 -0600

    add option to write file with primes

--- primefinder.py
+++ primefinder.py
@@ -10,4 +10,10 @@ def find_primes(num):
 
 limit = int(input("Enter the upper limit for determining primes: "))
 primeList = find_primes(limit)
-print(primeList)
+choice = input("Do you want me to display the numbers or save them to a file? Enter 'file' or 'display': ")
+if choice == 'file':
+    f = open(f"primelist_{limit}", "w")
+    f.write(str(primeList))
+    f.close()
+else:
+    print(primeList)
