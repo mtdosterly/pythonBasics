4ba8632a543b9e5a27d7cd25bbcd53f313ca1e97
mtdosterly <74225864+mtdosterly@users.noreply.github.com>
2020-11-19 23:29:19 -0600

    limit # of multipliers

--- sieve.py
+++ sieve.py
@@ -1,13 +1,13 @@
 def prime_sieve(limit):
     primes = [num for num in range(2,limit + 1)] #list of numbers to work on
     for test in primes[0:]:  #testing each number still in the list
-        for x in range(2,limit): #iterating through a multiplier
+        for x in range(2,int(limit/test)): #iterating through a multiplier
             if (test * x) in primes: # if multiple of confirmed prime in list
                 primes.remove(test * x)  #delete it
     return primes
 
 limit = int(input("Enter the upper limit for determining primes: "))
 primeList = prime_sieve(limit)
-f = open(f"primelist_{limit}.txt", "a")
+f = open(f"primelist_{limit}.txt", "w")
 f.write(str(primeList))
 f.close()
