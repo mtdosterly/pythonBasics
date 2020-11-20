6ccf292c28f72a31a9ed9765ee7f5b06e2ec312e
mtdosterly <74225864+mtdosterly@users.noreply.github.com>
2020-11-19 23:08:58 -0600

    remove print option

--- sieve.py
+++ sieve.py
@@ -7,11 +7,7 @@ def prime_sieve(limit):
     return primes
 
 limit = int(input("Enter the upper limit for determining primes: "))
-choice = input("Do you want me to display the numbers or save them to a file? Enter 'file' or 'display': ")
 primeList = prime_sieve(limit)
-if choice == 'file':
-    f = open(f"primelist_{limit}", "w")
-    f.write(str(primeList))
-    f.close()
-else:
-    print(primeList)
+f = open(f"primelist_{limit}.txt", "a")
+f.write(str(primeList))
+f.close()
